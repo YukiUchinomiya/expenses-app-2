9168b1b1d45da604d5cacdaa129bd83d479f6e33
YukiUchinomiya <1mm51f13385411f@gmail.com>
2022-07-08 16:35:58 +0900

    fix:微修正

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,6 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Post;
+use App\Models\User;
+
 use Illuminate\Http\Request;
 
 class UserController extends Controller
@@ -13,11 +16,20 @@ class UserController extends Controller
     ];
 
     public function index() {
+
+        //WIPデータ取得不備調査中
+        // $user = User::all();
+
         return view('contents.index');
     }
 
     public function practice() {
         return view('contents.practice')
             ->with(['posts' => $this->posts]);
+    }
+
+    public function update (){
+        return view('contents.detail')
+        ->with(['hogè' => $this->$_COOKIE]);
     }
 }
