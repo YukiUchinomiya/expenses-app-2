ba9e4d47abe0a4aa1df0afb768a8a5d3a306330f
YukiUchinomiya <1mm51f13385411f@gmail.com>
2022-09-09 16:40:51 +0900

    fix:js復習

--- public/script.js
+++ public/script.js
@@ -5,3 +5,17 @@ function submitAlert() {
     target = document.getElementById("submit-event");
     alert("送信しました！");
 }
+
+'use strict';
+
+function sum(a, b, c) {
+    // console.log(a + b + c);
+    return a + b + c;
+    console.log(a + b + c);
+}
+
+// sum(1, 2, 3);
+// sum(3, 4, 5);
+
+const total = sum(1, 2, 3) + sum(3, 4, 5); // 18
+// console.log(total);
